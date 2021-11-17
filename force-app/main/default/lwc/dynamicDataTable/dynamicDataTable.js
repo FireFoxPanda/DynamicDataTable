d84e671bfb773bf84df5719bc9a9bb92a14c9068
Prakhar <ranaprakhar97@gmail.com>
2021-11-17 23:42:21 +0530

    1st Build

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -1,8 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from "lwc";
+import getDatableInfo from "@salesforce/apex/DynamicDataTableHelper.getDatableInfo";
 
 export default class DynamicDataTable extends LightningElement {
+  sobjectName = "Account";
+  fieldList = [];
+  @track dataTableInfo;
+  @track dataTableRecords;
+  columns;
 
-sobjectName = 'Account';
-fieldList = [];
-
+  @wire(getDatableInfo, {
+    sobjApiName: this.sobjectName,
+    columnFields: this.fieldList
+  })
+  wiredData({ error, data }) {
+    if (data) {
+      this.dataTableInfo = data;
+      this.columns = data.lstDataTableColumnProperties;
+      this.dataTableRecords = data.lstDataTableRecords;
+    }
+  }
 }
