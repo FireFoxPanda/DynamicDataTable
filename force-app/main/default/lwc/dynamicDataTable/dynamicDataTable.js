8a7c35a63c655e6921a4d8da82805e6231171774
Prakhar <ranaprakhar@yahoo.in>
2021-11-18 02:12:02 +0530

    Basic Funtionality - {Sorting , Dynamic Columns}

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -6,6 +6,9 @@ export default class DynamicDataTable extends LightningElement {
   fieldList = "name,";
   @track dataTableInfo;
   @track dataTableRecords;
+  defaultSortDirection = "asc";
+  sortDirection = "asc";
+  sortedBy;
 
   columns;
 
@@ -19,5 +22,31 @@ export default class DynamicDataTable extends LightningElement {
       this.columns = data.lstDataTableColumnProperties;
       this.dataTableRecords = data.lstDataTableRecords;
     }
+  }
+
+  sortBy(field, reverse, primer) {
+    const key = primer
+      ? function (x) {
+          return primer(x[field]);
+        }
+      : function (x) {
+          return x[field];
+        };
+
+    return function (a, b) {
+      a = key(a);
+      b = key(b);
+      return reverse * ((a > b) - (b > a));
+    };
+  }
+
+  onHandleSort(event) {
+    const { fieldName: sortedBy, sortDirection } = event.detail;
+    const cloneData = [...this.dataTableRecords];
+    console.log(cloneData);
+    cloneData.sort(this.sortBy(sortedBy, sortDirection === "asc" ? 1 : -1));
+    this.dataTableRecords = cloneData;
+    this.sortDirection = sortDirection;
+    this.sortedBy = sortedBy;
   }
 }
